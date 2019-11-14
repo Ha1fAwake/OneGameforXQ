226a5630478186157bf492d0399eaec292d4fcc5
QuanMao <2541673770@qq.com>
2019-11-14 12:34:30 +0800

    Add Generator.cs and UI

---
+++ OneGameBelongstoXQ/Assets/Scripts/Controller.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : MonoBehaviour
+{
+    public float jumpForce;
+    public float walkForce;
+    public AudioClip cantStart;
+
+    private GameObject player;
+    private GameObject reward;
+    private bool start = false;
+
+    public void OnExit()
+    {
+        Application.Quit();
+    }
+
+    public void OnJump()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        reward = GameObject.FindGameObjectWithTag("Reward");
+
+        if (player == null && reward == null)
+        {
+            AudioSource.PlayClipAtPoint(cantStart, transform.position);
+        }
+
+        if (player!= null && reward != null)
+        {
+            start = true;
+        }
+
+        if (start)
+        {
+            if (player.GetComponent<Rigidbody2D>() == null)
+            {
+                player.AddComponent<Rigidbody2D>();
+                start = true;
+            }
+            else
+                player.GetComponent<Rigidbody2D>().AddForce(new Vector2(0, jumpForce));
+        }
+    }
+
+    public void OnLeft()
+    {
+        if (start)
+            player.GetComponent<Rigidbody2D>().AddForce(new Vector2(-walkForce, 0));
+    }
+    
+    public void OnRight()
+    {
+        if (start)
+            player.GetComponent<Rigidbody2D>().AddForce(new Vector2(walkForce, 0));
+    }
+}
---
+++ OneGameBelongstoXQ/Assets/Scripts/Ganerator.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/// <summary>
+/// 祝薛芹生日快乐！—— 来自wxx的祝福
+/// </summary>
+public class Ganerator : MonoBehaviour
+{// 在出现奖励前，触碰屏幕随机生成机关。一定触碰次数生成奖励和玩家
+    public GameObject[] layouts;    // 触碰屏幕可以生成的物体，包括：玩家（罗小黑）、陷阱、移动平台、奖励
+    public GameObject player;
+    public GameObject reward;
+    public int timeToGeneratePlayer;// 生成玩家的时机
+    public int timeToGenerateReward;// 生成奖励的时机
+
+    private int timer = 0;                  // 触屏次数
+    private Vector3 generatePosition;       // 生成位置
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            timer++;
+            generatePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            generatePosition.z = 0;
+            GameObject toGenerate = layouts[Random.Range(0, layouts.Length)];
+            if (timer == timeToGeneratePlayer)
+                toGenerate = player;
+            if (timer == timeToGenerateReward)
+                toGenerate = reward;
+            Instantiate(toGenerate, generatePosition, Quaternion.identity);
+        }
+    }
+}
